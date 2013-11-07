86672d43ac15778b739d1004cc42ba345bef8ecb
charly89 <karl-heinzstoeckler@gmx.at>
2013-11-07 14:04:47 +0100

    >Tag> Clemens: Logger Ausgabe angepasst

--- SortedSetExample/SortedSetExample/src/main/java/at/jku/ce/Main.java
+++ SortedSetExample/SortedSetExample/src/main/java/at/jku/ce/Main.java
@@ -10,11 +10,11 @@ public class Main {
 		private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
 		
 		Set<DomainObject> objectSet = new HashSet<DomainObject>(); 
-		objectSet.add(new DomainObject("Helga")); 
-		objectSet.add(new DomainObject("Walter")); 
-		objectSet.add(new DomainObject("Hans")); 
-		objectSet.add(new DomainObject("Paul")); 
-		objectSet.add(new DomainObject("Claudia")); 
+		objectSet.add(new DomainObject("Helga"), "Kommentar"); 
+		objectSet.add(new DomainObject("Walter"), "Kommentar"); 
+		objectSet.add(new DomainObject("Hans"), "Kommentar"); 
+		objectSet.add(new DomainObject("Paul"), "Kommentar"); 
+		objectSet.add(new DomainObject("Claudia"), "Kommentar"); 
 		
 		for (DomainObject domainObject : objectSet) { 
 			LOGGER.debug(domainObject.id); 
