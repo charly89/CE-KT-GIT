ff63fde8e27026f85bb238505cde928cbf37348f
charly89 <karl-heinzstoeckler@gmx.at>
2013-11-07 13:43:54 +0100

    <Tag4> Barbara: Konstruktor geändert

--- SortedSetExample/SortedSetExample/src/main/java/at/jku/ce/DomainObject.java
+++ SortedSetExample/SortedSetExample/src/main/java/at/jku/ce/DomainObject.java
@@ -5,7 +5,7 @@ public class DomainObject {
 	private String uuid =  UUID.randomUUID().toString();
 	private String name;
 
-	public DomainObject(String uuid) {
+	public DomainObject(String name, String comment) {
 		super();
 		this.uuid = uuid;
 	}
